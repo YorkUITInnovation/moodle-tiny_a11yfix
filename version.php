25830a4847e2b9e2a7eeaa9b44a9af2db099c101
Patrick Thibaudeau <thibaud@yorku.ca>
2026-02-17 08:27:13 -0500

    Updated version

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026021501;
+$plugin->version   = 2026021502;
 $plugin->requires  = 2025092600;
 $plugin->component = 'tiny_a11yfix';
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.8.0';
+$plugin->release   = '0.9.0';
 $plugin->dependencies = array(
     'aiplacement_a11y' => 2026021401,
 );
